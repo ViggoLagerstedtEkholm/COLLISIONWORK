20919cefa1c0128ff891738247fe5dada2ab1aca
ViggoLagerstedtEkholm <60555651+ViggoLagerstedtEkholm@users.noreply.github.com>
2020-07-24 16:50:11 +0200

    Added checkdead

--- COLLISIONWORK/GameObjects/Player.cs
+++ COLLISIONWORK/GameObjects/Player.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using COLLISIONWORK.World;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -9,24 +10,46 @@ namespace COLLISIONWORK.GameObjects
 {
     public class Player
     {
-        private Shape2D Shape;
+        public Shape2D Shape { get; set; }
         public int Health { get; set; }
-        public Player(Shape2D Shape, int Health)
+        private Sound sound;
+
+        public Player(Shape2D Shape, int Health, Sound sound, ShapeHandler shapeHandler)
         {
             this.Shape = Shape;
             this.Health = Health;
+            this.sound = sound;
         }
-
         public void Damage()
         {
             Health -= 1;
             Shape.Color = Color.Red;
+            CheckDead();
         }
 
         public void Heal()
         {
-            Health += 50;
-            Shape.Color = Color.Green;
+            if(Health != 200)
+            {
+                Health += 50;
+                if(Health > 200)
+                {
+                    Health = 200;
+                }
+                Shape.Color = Color.Green;
+            }
+        }
+
+        public bool CheckDead()
+        {
+            bool dead = false;
+
+            if(Health <= 0)
+            {
+                dead = true;
+            }
+
+            return dead;
         }
     }
 }
